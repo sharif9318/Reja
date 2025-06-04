47cd8fe0f5ae3617775755962b0aeb0448377701
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-05 04:16:14 +0900

    added author page

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
-
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Faylni o'qishda xatolik:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -14,12 +22,18 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
 app.get("/", function (req, res) {
-  res.end("<h1>HELLO WORLD</h1>");
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
-});
+server.listen(PORT, function () {});
