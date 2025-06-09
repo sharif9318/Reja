eb96e5c5fb7f788b2e46351eb5c6e5007e83ee9f
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-10 01:59:39 +0900

    MongoDB databease connected

--- server.js
+++ server.js
@@ -1,42 +1,28 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("Faylni o'qishda xatolik:", err);
-  } else {
-    user = JSON.parse(data);
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://sharifdilmuratovich:c7wL2zBnVDrKrHid@cluster0.aoxyoqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost: ${PORT}`
+        );
+      });
+    }
   }
-});
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  // TODO: code with db here
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`Server ${PORT} portda ishga tushdi`);
-  console.log("http://localhost:" + PORT);
-});
+);
