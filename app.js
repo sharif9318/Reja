b9a8f690e5a492706f7e4002819b7b4c6cd39f8e
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-13 06:39:04 +0900

    Reja:frontend.axios

--- app.js
+++ app.js
@@ -17,17 +17,13 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
+  console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    res.json(data.ops[0]);
   });
 });
+
 app.get("/", function (req, res) {
   console.log("user entered /");
   db.collection("plans")
