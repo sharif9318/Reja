4f57b6465df17b72ac5d6d6e5ebbffdba0f00a73
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-15 00:54:44 +0900

    CRUD:update

--- app.js
+++ app.js
@@ -36,6 +36,27 @@ app.post("/delete-item", (req, res) => {
   );
 });
 
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+  res.end("done");
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "hamma rejalar ochirildi" });
+    });
+  }
+});
+
 app.get("/", function (req, res) {
   console.log("user entered /");
   db.collection("plans")
